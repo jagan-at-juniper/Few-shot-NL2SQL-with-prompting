97bc42162d6ebb473d80c16ec74375c659c0f17e
Ryan Riopelle <ryan.riopelle@icloud.com>
2020-05-13 14:38:00 -0700

    added jenkins file

--- ryanr/jenkins_populate_action_data.sh
+++ ryanr/jenkins_populate_action_data.sh
@@ -1,30 +1,38 @@
 #!/bin/bash
 
-script_date=python -c "import datetime; datetime.date.fromordinal(datetime.date.today().toordinal()-1).strftime("%F")")
-echo $script_date
-#today = date.today()
-#yesterday = today - timedelta(days = 1)
+response=$(curl -H "Content-Type: application/json" -X POST http://ec2-3-93-213-4.compute-1.amazonaws.com:8998/batches -d '{"className": "org.apache.spark.examples.SparkPi", "args": ["blah"], "file": "s3://mist-data-science-dev/ryanr/action_report.py"}')
 
+echo "getting id"
+echo "$response" | python -c "import sys, json; print json.load(sys.stdin)['id']"
+session_id=$(echo "$response" | python -c "import sys, json; print json.load(sys.stdin)['id']")
+echo "session_id: $session_id"
 
-#yest=$(date --date="yesterday")
-#echo "$yest"
-#yest=$(date --date="yesterday" +"%d/%m/%Y")
-#echo "The backup was last verified on $yest"
+url_check_state="http://ec2-3-93-213-4.compute-1.amazonaws.com:8998/batches/${session_id}/state)"
+curl -X GET "$url_check_state"
 
-#curl -H "Content-Type: application/json" -X POST http://ec2-54-147-14-244.compute-1.amazonaws.com:8998/batches -d "$json_var"
-#
-#response=$(curl -H "Content-Type: application/json" -X POST http://ec2-54-147-14-244.compute-1.amazonaws.com:8998/batches -d "$json_var")
-#echo "getting id"
-#echo "$response" | python -c "import sys, json; print json.load(sys.stdin)['id']"
-#session_id=$(echo "$response" | python -c "import sys, json; print json.load(sys.stdin)['id']")
-#echo "session_id: $session_id"
-#
-#url_check_state="http://ec2-54-147-14-244.compute-1.amazonaws.com:8998/batches/${session_id}/state)"
-#curl -X GET "$url_check_state"
-#
-#response2=$(curl -X GET http://ec2-54-147-14-244.compute-1.amazonaws.com:8998/batches/${session_id}/state)
-#echo "response2: $response2"
-#
-#
-#final_state_response=$(echo "$response2" | python -c "import sys, json; print json.load(sys.stdin)['state']")
-#echo "final_state_response: $final_state_response"sponse=$(echo "$response2" | python -c "import sys, json; print json.load(sys.stdin)['state']")
+response2=$(curl -X GET http://ec2-3-93-213-4.compute-1.amazonaws.com:8998/batches/${session_id}/state)
+echo "response2: $response2"
+
+final_state_response=$(echo "$response2" | python -c "import sys, json; print json.load(sys.stdin)['state']")
+echo "final_state_response: $final_state_response"
+
+final_state_response=$(echo "$response2" | python -c "import sys, json; print json.load(sys.stdin)['state']")
+echo "final_state_response: $final_state_response"
+
+final_state_response=$(echo "$response2" | python -c "import sys, json; print json.load(sys.stdin)['state']")
+echo "final_state_response: $final_state_response"
+
+final_state_response=$(echo "$response2" | python -c "import sys, json; print json.load(sys.stdin)['state']")
+echo "final_state_response: $final_state_response"
+
+final_state_response=$(echo "$response2" | python -c "import sys, json; print json.load(sys.stdin)['state']")
+echo "final_state_response: $final_state_response"
+
+final_state_response=$(echo "$response2" | python -c "import sys, json; print json.load(sys.stdin)['state']")
+echo "final_state_response: $final_state_response"
+
+final_state_response=$(echo "$response2" | python -c "import sys, json; print json.load(sys.stdin)['state']")
+echo "final_state_response: $final_state_response"
+
+final_state_response=$(echo "$response2" | python -c "import sys, json; print json.load(sys.stdin)['state']")
+echo "final_state_response: $final_state_response"
