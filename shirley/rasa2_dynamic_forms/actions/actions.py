3e2775dcc64b2847819b6820d8c349568b36f7a0
shirley <swu@mistsys.com>
2020-12-12 00:17:39 -0800

    single path works

--- shirley/rasa2_dynamic_forms/actions/actions.py
+++ shirley/rasa2_dynamic_forms/actions/actions.py
@@ -18,8 +18,7 @@ class ValidateTbshootForm(FormValidationAction):
     ) -> None:
         self.org_id = None
         self.form_state = {}
-        self.returned_slots = ['mac']
-        self.first_call = False
+        self.returned_slots = []
 
         self.condition = 0
         self.count = 0
@@ -58,54 +57,103 @@ class ValidateTbshootForm(FormValidationAction):
 
         count = self.form_state.get('count', 0)
         intent = tracker.latest_message.get('intent', {}).get('name', '')
-        required_slots = self.form_state.get('required_slots', ['mac'])
+        required_slots = self.form_state.get('required_slots', [])
         logger.info('Form intent: {}'.format(intent))
+        logger.info('Form state: {}'.format(self.form_state))
 
         if intent == 'deny':
             return []
 
-        self.first_call = not self.first_call
-        if self.first_call:
+        if self.condition == 0:
             # retrieve slot names which need to extract user input
             logger.info('1st call, return default slots {}'.format(required_slots))
+
+            if 'required_slots' not in self.form_state or not self.form_state['required_slots']:
+                self.form_state['required_slots'] = slots_mapped_in_domain
+                required_slots = self.form_state['required_slots']
+
+            self.form_state['count'] = count
+            self.condition += 1
+            logger.info('update form_state: {}'.format(self.form_state))
+            logger.info('END of RS\n\n')
             return required_slots
 
-        else:
+        elif self.condition == 2:
+            # retrieve slot names which need to extract user input
+            logger.info('3rd call, return slots {}'.format(required_slots))
+
+            self.form_state['required_slots'] = required_slots
+            self.form_state['count'] = count
+            self.condition = 0
+            logger.info('update form_state: {}'.format(self.form_state))
+            logger.info('END of RS\n\n')
+            return required_slots
+
+        elif self.condition == 1:
             logger.info('2nd call, checkout counter: {}'.format(count))
 
-            if count >= 3:
+            if count >= 30:
 
                 required_slots = []
                 self.form_state = dict()
             else:
 
                 count += 1
+                status = self.check_form_slots(tracker, required_slots)
+                logger.info('2nd call, extraction status: {}'.format(status))
+
                 required_slots = []
+                if count == 1 and status == 0:
 
-                extracted_mac = tracker.slots.get('mac', '')
-                logger.info('2nd call, extracted_mac: {}'.format(extracted_mac))
-
-                if count == 1 and extracted_mac is None:
-
-                    required_slots.append('mac')
-                elif count > 1 and extracted_mac is None:
+                    required_slots = ['mac']
+                elif count > 1 and status == 0:
                     dispatcher.utter_message('I can not find the mac. Next ask for hostname')
-                    required_slots.append('hostname')
+                    required_slots = ['hostname']
 
-                elif extracted_mac:
+                elif 2 <= status < 5:
 
-                    check, formatted_mac = check_mac_format(extracted_mac)
-                    if not check:
-                        required_slots.append('hostname')
+                    dispatcher.utter_message('I found multiple. What is the site name')
+                    dispatcher.utter_message('Please select one to continue.')
+                    required_slots = []
+
+                elif 5 <= status:
+                    dispatcher.utter_message('I found multiple. What is the site name')
+                    required_slots = ['sitename']
+                    
+                elif status == 1:
+                    dispatcher.utter_message('Yes, Found the device')
+                    required_slots = []
 
             # save all form state into variable form_state and will be
             # persist into slot later
             self.form_state['required_slots'] = required_slots
             self.form_state['count'] = count
-            logger.info('2nd call, update form_state: {}'.format(self.form_state))
+            self.condition += 1
+            logger.info('update form_state: {}'.format(self.form_state))
 
-        logger.info('END of RS')
-        return required_slots
+            logger.info('END of RS {}\n\n'.format(required_slots))
+            return required_slots
+
+    def reset_form_state(self):
+        self.form_state = dict()
+
+    def check_form_slots(self, tracker, returned_slots):
+
+        # based on form state and current user filled in slots values
+        # to validate the data
+
+        slot = returned_slots[0]
+        logger.info('what is the name of the slot? {}'.format(slot))
+        extracted_value = tracker.slots.get(slot, '')
+        logger.info('slot = {} value = {}'.format(slot, extracted_value))
+        if extracted_value == 'shirley_2':
+            return 2
+        elif extracted_value == 'shirley_1':
+            return 1
+        elif extracted_value == 'shirley_3':
+            return 3
+        else:
+            return 0
 
 
     async def extract_hostname(
@@ -150,6 +198,7 @@ class ValidateTbshootForm(FormValidationAction):
     ) -> Dict[Text, Any]:
         """Validate cuisine value."""
 
+        logger.info('validate_mac {}'.format(slot_value))
         check, formatted_mac = check_mac_format(slot_value)
         if check:
             return {"mac": formatted_mac}
