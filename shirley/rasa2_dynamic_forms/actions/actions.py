1ec493a022ed524d2b4032096dd5a1183c12af50
shirley <swu@mistsys.com>
2020-12-13 11:05:23 -0800

    utilize validate_xxx API to implement the dynamic diag flow

--- shirley/rasa2_dynamic_forms/actions/actions.py
+++ shirley/rasa2_dynamic_forms/actions/actions.py
@@ -55,106 +55,111 @@ class ValidateTbshootForm(FormValidationAction):
 
         logger.info('START of RS {}'.format(self.form_state))
 
-        count = self.form_state.get('count', 0)
         intent = tracker.latest_message.get('intent', {}).get('name', '')
-        required_slots = self.form_state.get('required_slots', [])
         logger.info('Form intent: {}'.format(intent))
         logger.info('Form state: {}'.format(self.form_state))
 
         if intent == 'deny':
+            # User does not want to continue, get out the form
+            dispatcher.utter_message('It seems you do not want to continue this topic. Stop form')
+            return []
+        elif not self.form_state:
+            # User just enters the form, return the default required slots
+            self.form_state['asking_slots'] = ['mac']
+            return self.form_state['asking_slots']
+
+        elif 'asking_slots' in self.form_state and not self.form_state.get('asking_slots'):
+            # In the form state, it has all the required information and
+            # validation method determine it's time to get out
             return []
 
-        if self.condition == 0:
-            # retrieve slot names which need to extract user input
-            logger.info('1st call, return default slots {}'.format(required_slots))
+        elif 'asking_slots' in self.form_state and self.form_state['asking_slots']:
+            # The from stats has slot names in 'asking_slots' field
+            # if the the first one is an empty slot, continue ask user
+            # otherwise, end the form
 
-            if 'required_slots' not in self.form_state or not self.form_state['required_slots']:
-                self.form_state['required_slots'] = slots_mapped_in_domain
-                required_slots = self.form_state['required_slots']
+            slots_check = self.form_state['asking_slots']
+            logger.info('checking slot {}'.format(slots_check))
+            empty_ones = list(filter(lambda s: tracker.get_slot(s) is None, slots_check))
 
-            self.form_state['count'] = count
-            self.condition += 1
-            logger.info('update form_state: {}'.format(self.form_state))
-            logger.info('END of RS\n\n')
-            return required_slots
-
-        elif self.condition == 2:
-            # retrieve slot names which need to extract user input
-            logger.info('3rd call, return slots {}'.format(required_slots))
-
-            self.form_state['required_slots'] = required_slots
-            self.form_state['count'] = count
-            self.condition = 0
-            logger.info('update form_state: {}'.format(self.form_state))
-            logger.info('END of RS\n\n')
-            return required_slots
-
-        elif self.condition == 1:
-            logger.info('2nd call, checkout counter: {}'.format(count))
-
-            if count >= 30:
-
-                required_slots = []
-                self.form_state = dict()
+            if len(empty_ones) == 0:
+                self.form_state.pop('asking_slots')
+                logger.info('End the form!!!!')
+                return []
             else:
+                return self.form_state['asking_slots']
 
-                count += 1
-                status = self.check_form_slots(tracker, required_slots)
-                logger.info('2nd call, extraction status: {}'.format(status))
+    def validate_mac(
+        self,
+        slot_value: Any,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: DomainDict,
+    ) -> Dict[Text, Any]:
+        """Validate cuisine value."""
 
-                required_slots = []
-                if count == 1 and status == 0:
-
-                    required_slots = ['mac']
-                elif count > 1 and status == 0:
-                    dispatcher.utter_message('I can not find the mac. Next ask for hostname')
-                    required_slots = ['hostname']
-
-                elif 2 <= status < 5:
-
-                    dispatcher.utter_message('I found multiple. What is the site name')
-                    dispatcher.utter_message('Please select one to continue.')
-                    required_slots = []
-
-                elif 5 <= status:
-                    dispatcher.utter_message('I found multiple. What is the site name')
-                    required_slots = ['sitename']
-                    
-                elif status == 1:
-                    dispatcher.utter_message('Yes, Found the device')
-                    required_slots = []
-
-            # save all form state into variable form_state and will be
-            # persist into slot later
-            self.form_state['required_slots'] = required_slots
-            self.form_state['count'] = count
-            self.condition += 1
-            logger.info('update form_state: {}'.format(self.form_state))
-
-            logger.info('END of RS {}\n\n'.format(required_slots))
-            return required_slots
-
-    def reset_form_state(self):
-        self.form_state = dict()
-
-    def check_form_slots(self, tracker, returned_slots):
-
-        # based on form state and current user filled in slots values
-        # to validate the data
-
-        slot = returned_slots[0]
-        logger.info('what is the name of the slot? {}'.format(slot))
-        extracted_value = tracker.slots.get(slot, '')
-        logger.info('slot = {} value = {}'.format(slot, extracted_value))
-        if extracted_value == 'shirley_2':
-            return 2
-        elif extracted_value == 'shirley_1':
-            return 1
-        elif extracted_value == 'shirley_3':
-            return 3
+        logger.info('validate_mac {}'.format(slot_value))
+        check, formatted_mac = check_mac_format(slot_value)
+        if check:
+            return {"mac": formatted_mac}
+        elif self.form_state.get('mac'):
+            return {"mac": self.form_state.get('mac')}
         else:
-            return 0
+            dispatcher.utter_message('Can not find device of this mac. I will ask user for something else \n\n')
+            self.form_state['asking_slots'] = ['hostname']
+            return {"mac": None}
 
+    def validate_hostname(
+        self,
+        slot_value: Any,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: DomainDict,
+    ) -> Dict[Text, Any]:
+        """Validate cuisine value."""
+
+        logger.info('validate_mac {}'.format(slot_value))
+        count = find_hostname(slot_value)
+        if count == 1:
+            dispatcher.utter_message('Perfect! Found one host')
+            dispatcher.utter_message('Stop the form \n')
+            self.form_state['asking_slots'] = []
+            return {"hostname": slot_value}
+        elif 2 <= count < 5:
+            # TODO
+            dispatcher.utter_message('Found multiple devices match the name. Please click one from the list..')
+            dispatcher.utter_message('Stop the form \n')
+            self.form_state['asking_slots'] = []
+            return {"hostname": None}
+        else:
+            dispatcher.utter_message('Can not find device of this name. I will ask user for something else \n')
+            self.form_state['asking_slots'] = ['sitename']
+            return {"hostname": None}
+
+    def validate_sitename(
+        self,
+        slot_value: Any,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: DomainDict,
+    ) -> Dict[Text, Any]:
+        """Validate cuisine value."""
+
+        logger.info('validate_mac {}'.format(slot_value))
+        count = find_sitename(slot_value)
+        if count == 1:
+            self.form_state['asking_slots'] = []
+            return {"sitename": slot_value}
+        elif 2 <= count < 5:
+            # TODO
+            dispatcher.utter_message('Found multiple sites match the name: mist0, mist1, mist2... which is the one?')
+            self.form_state['asking_slots'] = []
+            return {"sitename": None}
+        else:
+            dispatcher.utter_message('Hmm, can not pinpoint the site for the given name. '
+                                     'Maybe I should redirect you to different page?')
+            self.form_state['asking_slots'] = []
+            return {"sitename": None}
 
     async def extract_hostname(
         self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict
@@ -177,7 +182,7 @@ class ValidateTbshootForm(FormValidationAction):
         sitename = tracker.get_slot('sitename')
 
         return {"sitename": sitename}
-    
+
     async def extract_deviceType(
         self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict
     ) -> Dict[Text, Any]:
@@ -189,21 +194,54 @@ class ValidateTbshootForm(FormValidationAction):
 
         return {"deviceType": deviceType}
 
-    def validate_mac(
-        self,
-        slot_value: Any,
-        dispatcher: CollectingDispatcher,
-        tracker: Tracker,
-        domain: DomainDict,
-    ) -> Dict[Text, Any]:
-        """Validate cuisine value."""
 
-        logger.info('validate_mac {}'.format(slot_value))
-        check, formatted_mac = check_mac_format(slot_value)
-        if check:
-            return {"mac": formatted_mac}
-        else:
-            return {"mac": None}
+def validate(tracker, org_id, config, form_state, slot_name, slot_value):
+    """
+    Generic validation method:
+
+    entity_type: client
+
+    entity_search_flow:
+
+        - entity_type: client
+          entity_property: mac
+          enforce_single_match: True
+
+        - entity_type: client
+          entity_property: hostname
+          user_input:
+
+        - entity_type: site
+          entity_property: sitename
+          enforce_single_match: True
+          retries_if_empty: 2
+
+        - entity_type: client
+          entity_property: deviceType
+
+    :param tracker:
+    :param disorg_id:
+    :param config:
+    :param form_state:
+    :return:
+    """
+    form_config = form_state.get('form_control')
+    current_flow_inx = form_state.get('form_control')
+    current_flow = form_config.get('entity_search_flow')[current_flow_inx]
+
+    # a dict of user validated user inputs
+    # which may contain the site_id (based user inputted side_name)
+    # AP mac (based on user inputted ap name)
+    # etc
+    validated_user_inputs = form_state['validated_user_inputs']
+
+    # compose entity_search based on previous validated user inputs and current slot value
+    # to validate current slot value
+
+
+    # Based on current validated slot value to search the entity required by the form
+    pass
+
 
 def check_mac_format(name):
     if re.match("[0-9a-f]{2}([-:]?)[0-9a-f]{2}(\\1[0-9a-f]{2}){4}$", name.lower()):
@@ -212,6 +250,7 @@ def check_mac_format(name):
     return False, name
 
 def find_hostname(hostname):
+    number_found = 5
     if hostname == 'amin0':
         number_found = 0
     if hostname == 'amin1':
@@ -221,6 +260,7 @@ def find_hostname(hostname):
     return number_found
 
 def find_sitename(sitename):
+    number_found = 5
     if sitename == 'mist0':
         number_found = 0
     if sitename == 'mist1':
