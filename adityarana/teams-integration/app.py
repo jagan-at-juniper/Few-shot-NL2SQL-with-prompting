1d2deeaa555a347e8a98bdfa3e7b41517c461e05
Aditya Rana <adityarana@juniper.net>
2022-10-05 03:26:23 +0530

    teams web app with flask

--- adityarana/teams-integration/app.py
+++ adityarana/teams-integration/app.py
@@ -1,10 +1,13 @@
 import sys
 import traceback
-from datetime import datetime
-from http import HTTPStatus
 
-from aiohttp import web
-from aiohttp.web import Request, Response, json_response
+from botbuilder.core import (
+    BotFrameworkAdapterSettings,
+    TurnContext,
+    BotFrameworkAdapter,
+)
+from botbuilder.schema import Activity, ActivityTypes
+
 from botbuilder.core import (
     BotFrameworkAdapter,
     BotFrameworkAdapterSettings,
@@ -13,10 +16,11 @@ from botbuilder.core import (
     TurnContext,
     UserState,
 )
-from botbuilder.core.integration import aiohttp_error_middleware
 from botbuilder.schema import Activity, ActivityTypes
+from aioflask import Flask, request, jsonify
 
 from bots import TeamsBot
+from utils import DEFAULT_RESPONSES
 
 # Create the loop and Flask app
 from config import DefaultConfig
@@ -34,24 +38,8 @@ async def on_error(context: TurnContext, error: Exception):
     print(f"\n [on_turn_error] unhandled error: {error}", file=sys.stderr)
     traceback.print_exc()
 
-    await context.send_activity("The bot encountered an error or bug.")
-    await context.send_activity(
-        "To continue to run this bot, please fix the bot source code."
-    )
-
-    # Send a trace activity if we're talking to the Bot Framework Emulator
-    if context.activity.channel_id == "emulator":
-        # Create a trace activity that contains the error object
-        trace_activity = Activity(
-            label="TurnError",
-            name="on_turn_error Trace",
-            timestamp=datetime.utcnow(),
-            type=ActivityTypes.trace,
-            value=f"{error}",
-            value_type="https://www.botframework.com/schemas/error",
-        )
-        await context.send_activity(trace_activity)
-
+    # Send a message to the user
+    await context.send_activity(DEFAULT_RESPONSES["error"])
 
 ADAPTER.on_turn_error = on_error
 
@@ -66,31 +54,36 @@ DIALOG = MainDialog(CONFIG.CONNECTION_NAME)
 # Create Bot
 BOT = TeamsBot(CONVERSATION_STATE, USER_STATE, DIALOG)
 
+# Creating APP
+app = Flask(__name__)
 
-# Listen for incoming requests on /api/messages.
-async def messages(req: Request) -> Response:
+@app.route("/api/messages", methods=['POST'])
+async def message():
     print("running...")
     # Main bot message handler.
-    if "application/json" in req.headers["Content-Type"]:
-        body = await req.json()
+    if "application/json" in request.headers["Content-Type"]:
+        body = request.get_json()
     else:
-        return Response(status=HTTPStatus.UNSUPPORTED_MEDIA_TYPE)
+        resp = jsonify(
+                    message="Unsupported media type",
+                    category="error",
+                    status=415
+                )
+        return resp
 
     activity = Activity().deserialize(body)
-    auth_header = req.headers["Authorization"] if "Authorization" in req.headers else ""
+    auth_header = request.headers["Authorization"] if "Authorization" in request.headers else ""
 
-    invoke_response = await ADAPTER.process_activity(activity, auth_header, BOT.on_turn)
-    if invoke_response:
-        return json_response(data=invoke_response.body, status=invoke_response.status)
+    await ADAPTER.process_activity(activity, auth_header, BOT.on_turn)
     print("done...")
-    return Response(status=HTTPStatus.OK)
+    return jsonify(success=True)
 
-
-APP = web.Application(middlewares=[aiohttp_error_middleware])
-APP.router.add_post("/api/messages", messages)
+@app.route("/", methods=['GET'])
+def greet():
+    return "Hello World!"
 
 if __name__ == "__main__":
     try:
-        web.run_app(APP, host="localhost", port=CONFIG.PORT)
+        app.run(debug=True, host=CONFIG.HOST_NAME, port=CONFIG.PORT)
     except Exception as error:
         raise error
--- adityarana/teams-integration/config.py
+++ adityarana/teams-integration/config.py
@@ -1,6 +1,7 @@
 import os
 
 class DefaultConfig:
+    HOST_NAME="0.0.0.0"
     PORT = 5000
     APP_ID = os.environ.get("MicrosoftAppId", "")
     APP_PASSWORD = os.environ.get("MicrosoftAppPassword", "")
